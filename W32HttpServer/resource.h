c49022a4d1d34d381020051bbd0f9e44e99e0dbc
Didiet <lynxluna@gmail.com>
2022-11-15 23:36:40 +0700

    No RC editor allowed
    All resource script should be written by hand, like real programmer

--- W32HttpServer/resource.h
+++ W32HttpServer/resource.h
@@ -1,18 +1,10 @@
-//{{NO_DEPENDENCIES}}
-// Microsoft Developer Studio generated include file.
-// Used by W32HttpServer.rc
-//
+#include <windows.h>
+
+
+#define IDC_STATIC -1 
+#define ID
+
 #define IDI_HTTP                        101
 #define IDS_MAIN_WINDOW_TITLE           103
 #define IDI_HEAD                        105
 
-// Next default values for new objects
-// 
-#ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        106
-#define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
-#define _APS_NEXT_SYMED_VALUE           101
-#endif
-#endif
